49ac3a8adfc4535d557b36f6c8b9c18585ba45cd
Pournima-Gawhane <52081825+Pournima-Gawhane@users.noreply.github.com>
2026-08-11 04:30:08 +0530

    Add project files.

---
+++ CorAppForJenkins/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public void Add()
+    {
+        Console.WriteLine("calling Add");
+
+    }
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.Add();
+    }
+}
